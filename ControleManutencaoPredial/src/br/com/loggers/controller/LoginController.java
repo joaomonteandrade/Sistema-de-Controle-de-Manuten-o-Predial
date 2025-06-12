0cac42df321e8e6abb2201386224f5a8556cb30f
joaomonteandrade <joaomontea@gmail.com>
2025-06-12 19:21:11 -0400

    qualquercoisa

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/LoginController.java
@@ -0,0 +1,39 @@
+
+package br.com.loggers.controller;
+
+import br.com.loggers.model.Connect;
+import br.com.loggers.model.LoginDAO;
+import br.com.loggers.view.View;
+import br.com.loggers.controller.User;
+
+import java.sql.SQLException;
+import java.sql.Connection;
+
+
+
+public class LoginController {
+    
+    /**public void cadastroUsuario(View view) throws SQLException{
+        Connection conexao = new Connect().conectar();
+        LoginDAO cadastro = new LoginDAO();
+        cadastro.cadastrarUsuario(view.getjTextField3().getText(), view.getjTextField2().getText(), view.getjPasswordField2().getText());
+        
+    }**/
+    
+    
+
+    User user = new User();
+
+    public boolean loginUsuario(View view) throws SQLException {
+        Connection conexao = new Connect().conectar();
+        LoginDAO login = new LoginDAO();
+        if(login.login(view.getjTextFieldEmail().getText(), view.getjPasswordFieldSenha().getText())){
+            String[] data = login.getData(view.getjTextFieldEmail().getText());
+            user.setData(data[0], data[1], data[2]);
+            System.out.println(user.getNome());
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/User.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.controller;
+
+/**
+ *
+ * @author Aluno
+ */
+public class User {
+    String nome, email, senha;
+
+    public void setData(String nome, String email, String senha){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    
+    
+    
+}
